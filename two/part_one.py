5b7973c58bb11ac2e35b2148658ca5b7456f681c
Gareth Coles <gdude2002@gmail.com>
2019-12-02 17:59:28 +0000

    Day 2, Python.

---
+++ two/part_one.py
@@ -0,0 +1,38 @@
+from typing import List
+
+memory: List[int]
+
+
+with open("data.txt") as fh:
+    memory = [
+        int(x) for x in fh.read().strip().replace("\n", "").split(",")
+    ]
+
+
+memory_iter = iter(memory)
+
+print("Initial state:", memory)
+
+for i, operation in enumerate(memory_iter):
+
+    if operation in (1, 2):
+        left = next(memory_iter, None)
+        right = next(memory_iter, None)
+        position = next(memory_iter, None)
+
+        if operation == 1:
+            memory[position] = memory[left] + memory[right]
+        elif operation == 2:
+            memory[position] = memory[left] * memory[right]
+    elif operation == 99:
+        break
+    else:
+        left = next(memory_iter, None)
+        right = next(memory_iter, None)
+        position = next(memory_iter, None)
+
+        print(f"Unknown opcode: {operation}")
+
+    print(f"{i} ({operation}) =>", memory)
+
+print(f"Position 0: {memory[0]}")
---
+++ two/part_two.py
@@ -0,0 +1,56 @@
+from typing import List
+
+orig_memory: List[int]
+
+with open("data.txt") as fh:
+    orig_memory = [
+        int(x) for x in fh.read().strip().replace("\n", "").split(",")
+    ]
+
+
+def attempt(noun: int, verb: int):
+    memory = orig_memory.copy()
+
+    memory[1], memory[2] = noun, verb
+
+    memory_iter = iter(memory)
+
+    for i, operation in enumerate(memory_iter):
+
+        if operation in (1, 2):
+            left = next(memory_iter, None)
+            right = next(memory_iter, None)
+            position = next(memory_iter, None)
+
+            if operation == 1:
+                memory[position] = memory[left] + memory[right]
+            elif operation == 2:
+                memory[position] = memory[left] * memory[right]
+        elif operation == 99:
+            break
+        else:
+            left = next(memory_iter, None)
+            right = next(memory_iter, None)
+            position = next(memory_iter, None)
+
+    return memory[0]
+
+
+wanted = 19690720
+result = 0
+
+
+def run():
+    for i in range(99):
+        for j in range(99):
+            result = attempt(i, j)
+
+            print(f"{i}, {j} -> {result}")
+
+            if wanted == result:
+                print("Found noun/verb combo:", i, j)
+                print("Result:", 100 * i + j)
+                return
+
+
+run()
